cf43a2fb7bb1f91875079d8e71cb884313e7ca9c
DiSoJi <diegosolisj@gmail.com>
2018-09-27 22:02:46 -0600

    Modifications to the ALU and it's testbench. Start of the control unit

--- src/ALU.sv
+++ src/ALU.sv
@@ -1,61 +1,77 @@
-module ALU(input logic [31:0] inputA,inputB,
+module ALU(input logic [31:0] inputA,
+				input logic [31:0] inputB,
 				input logic [3:0] ALU_control,
 				output logic [3:0] ALU_flags,
 				output logic [31:0] outputC
 );
 
-logic [32:0] temp_result;
-logic [3:0] temp_flags;
+logic [31:0] buffer;
+logic [31:0] SUM;
+logic [31:0] SUB;
+logic [31:0] Mult;
+logic [31:0] DIV;
+logic [31:0] SL;
+logic [31:0] SR;
+logic [31:0] SPSUM;
+
+assign buffer = inputA;
+assign SUM = inputA + inputB;
+assign SUB = inputA-inputB;
+assign Mult = inputA*inputB;
+assign DIV = inputA/inputB;
+assign SL = inputA<<inputB;
+assign SR = inputA>>inputB;
+assign SPSUM = inputA[7:0] + inputA[15:8] + inputA[23:16];
 
 always_comb
+begin
 		case(ALU_control)
+		
 		4'b0000://Case buffer
-			temp_result = inputA;
+			outputC = buffer;
 		4'b0001://Case sum
-			temp_result = inputA + inputB;
+			outputC = SUM;
 		4'b0010://Case substraction
-			temp_result = inputA - inputB;
+			outputC = SUB;
 		4'b0011://Case multiply
-			temp_result = inputA * inputB;
+			outputC = Mult;
 		4'b0100://Case divide
-			temp_result = inputA/inputB;
+			outputC = DIV;
 		4'b0101://Case Shift Left
-			temp_result = inputA<<inputB;
+			outputC = SL;
 		4'b0110://Case shift Right
-			temp_result = inputA>>inputB;
+			outputC = SR;
 		4'b0111://Case special sum
-			temp_result = inputA[7:0] + inputA[15:8] + inputA[23:16];
+			outputC = SPSUM;
 			
-		default: temp_result = 0;
+		default: outputC = buffer;
 		endcase
-
+		
+end
 always_comb //Falta agregar overflow
-	if (temp_result == 0)
+begin
+	if (outputC == 0)
 		begin
-		temp_flags = 4'b0100;
+		ALU_flags = 4'b0100;
 		end
 	else
 		begin
-			if (temp_result[32] == 1'b1)
+			if (outputC[31] == 1'b1)
 				begin
 					if (ALU_control == 4'b0010)
 						begin
-						temp_flags = 4'b1000;
+						ALU_flags = 4'b1000;
 						end
 					else
 						begin
-						temp_flags = 4'b0010;
+						ALU_flags = 4'b0010;
 						end
 				end
 			else
 				begin
-				temp_flags = 4'b0000;
+				ALU_flags = 4'b0000;
 				end
 		end
-
-		
-assign outputC = temp_result;
-assign ALU_flags = temp_flags;
-
+end
 endmodule
 			
