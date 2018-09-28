8fd6959f36db76e30bea2b5a83461bb1d87da5b7
prodrigu <G2014084649h>
2018-09-27 23:54:25 -0600

    arm add

---
+++ arm/arm.sv
@@ -0,0 +1,11 @@
+module arm(
+    input logic clk, reset,
+    output logic [31:0] PC,
+    input logic [31:0] Instr,
+    output logic MemWrite,
+    output logic [31:0] ALUResult, WriteData,
+    input logic [31:0] ReadData
+);
+
+
+endmodule
