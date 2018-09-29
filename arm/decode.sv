2ff09e686e8b4a7bcc8543f31e14af51155bedf2
jrodriguezs92 <jrodriguezs0292@gmail.com>
2018-09-28 22:33:18 -0600

    decode y modificacion a pipeDecoExe

--- arm/decode.sv
+++ arm/decode.sv
@@ -1,52 +1,55 @@
-module decode(input logic [1:0] Op,
-                input logic [5:0] Funct,
-                input logic [3:0] Rd,
-                output logic [1:0] FlagW,
-                output logic PCS, RegW, MemW,
-                output logic MemtoReg, ALUSrc,
-                output logic [1:0] ImmSrc, RegSrc, ALUControl
-);
+module decode(input  logic			 Clk, Rst, RegSrcD, ImmSrcD, RegWriteW, PCSrcD, RegWriteD, MemToRegD, MemToWriteD, ALUControlD, BranchD, ALUSrcD, FlagWriteD, Flags, 
+				  input  logic [31:0] Instruction, ResultW, PCPlus8D,
+				  input  logic [3:0]	 WA3W,
+				  output logic [3:0]  WA3E,
+				  output logic [31:0] RD1, RD2, Extended,
+				  output logic 		 PCSrcE, RegWriteE, MemToRegE, MemWriteE, ALUControlE, BranchE, ALUSrcE, FlagWriteE, CondEPipeOutput, FlagsE);
 
-logic [9:0] controls;
-logic Branch, ALUOp;
+				  
+	//Instruction Mapping
+	
+	logic [1:0]  Op;
+	logic [5:0]  Funct;
+	logic [3:0]  Rd, CondE, decodeMux2x1ToRA1D_IN1, decodeMux2x1ToRA2D_IN1, decodeMux2x1ToRA2D_IN2;
+	logic [23:0] ExtendedIN;
+	
+	assign Op 	 						= Instruction[27:26];
+	assign Funct 						= Instruction[25:20];
+	assign Rd    						= Instruction[15:12];
+	assign CondE 						= Instruction[31:28];
+	assign decodeMux2x1ToRA1D_IN1 = Instruction[19:16];
+	assign decodeMux2x1ToRA2D_IN1 = Instruction[3:0];
+	assign decodeMux2x1ToRA2D_IN2 = Instruction[15:12];
+	assign ExtendedIN 				= Instruction[23:0];
+	
+	//Outputs
+	
+	logic [3:0]  decodeMux2x1ToRA1D_Output, decodeMux2x1ToRA2D_Output, WA3E_Output;
+	logic [31:0] decodeExtended_Output, decodeRD1_Output, decodeRD2_Output, decodePipeRA_Output, decodePipeRB_Output, decodePipeExtended_Output;
 
-// Main Decoder
-always_comb
-    casex(Op)
-        // Data-processing immediate
-        2'b00: if (Funct[5]) controls = 10'b0000101001;
-        // Data-processing register
-        else controls = 10'b0000001001;
-        // LDR
-        2'b01: if (Funct[0]) controls = 10'b0001111000;
-        // STR
-        else controls = 10'b1001110100;
-        // B
-        2'b10: controls = 10'b0110100010;
-        // Unimplemented
-        default: controls = 10'bx;
-    endcase
+	//Components
+	
+	mux2x1
+	#(4)
+	decodeMux2x1ToRA1D(decodeMux2x1ToRA1D_IN1, 15, RegSrcD, decodeMux2x1ToRA1D_Output);
+	
+	mux2x1
+	#(4)
+	decodeMux2x1ToRA2D(decodeMux2x1ToRA2D_IN1, decodeMux2x1ToRA2D_IN2, RegSrcD, decodeMux2x1ToRA2D_Output);
+	
+	extend
+	extendUnit(ExtendedIN, ImmSrcD, decodeExtended_Output);
+	
+	registerFile
+	registerFileUnit(Clk, RegWriteW, decodeMux2x1ToRA1D_Output, decodeMux2x1ToRA2D_Output, WA3W, ResultW, PCPlus8D, decodeRD1_Output, decodeRD2_Output);
 
-assign {RegSrc, ImmSrc, ALUSrc, MemtoReg,
-RegW, MemW, Branch, ALUOp} = controls;
-// ALU Decoder
-always_comb
-    if (ALUOp) begin // which DP Instr?
-        case(Funct[4:1])
-            4'b0100: ALUControl = 2'b00; // ADD
-            4'b0010: ALUControl = 2'b01; // SUB
-            4'b0000: ALUControl = 2'b10; // AND
-            4'b1100: ALUControl = 2'b11; // ORR
-            default: ALUControl = 2'bx; // unimplemented
-        endcase
-        // update flags if S bit is set (C & V only for arith)
-        FlagW[1] = Funct[0];
-        FlagW[0] = Funct[0] &
-        (ALUControl == 2'b00 | ALUControl == 2'b01);
-    end else begin
-        ALUControl = 2'b00; // add for non-DP instructions
-        FlagW = 2'b00; // don't update Flags
-    end
-// PC Logic
-assign PCS = ((Rd == 4'b1111) & RegW) | Branch;
-endmodule
+	pipeDecoExe
+	pipeDE(Clk, Rst, decodeRD1_Output, decodePipeRA_Output, decodeRD2_Output, decodePipeRB_Output, decodeExtended_Output, decodePipeExtended_Output, decodeMux2x1ToRA2D_IN2, WA3E_Output);
+	
+	//Set output signals
+	assign WA3E  	 = WA3E_Output;
+	assign RD1  	 = decodePipeRA_Output;
+	assign RD2 		 = decodePipeRB_Output;
+	assign Extended = decodePipeExtended_Output;
+
+endmodule 
