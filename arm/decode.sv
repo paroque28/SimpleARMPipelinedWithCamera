08d8b54b26d10daba2f66397711b2f3b14596630
jrodriguezs92 <jrodriguezs0292@gmail.com>
2018-09-29 03:27:45 -0600

    decode arreglado

--- arm/decode.sv
+++ arm/decode.sv
@@ -1,25 +1,22 @@
-module decode(input  logic			 Clk, Rst, RegWriteW, ALUSrcEin, RegWriteDin, PlusOnein, MemSelEin, //Agregar ALUSrcE, RegWriteD, PlusOne, MemSelE a pipe
+module decode(input  logic			 Clk, Rst, RegWriteW,
 				  input  logic [31:0] Instruction, ResultW, PCPlus8D,
-				  input  logic [3:0]	 WA3W, ALUControlEin,
-				  input  logic [4:0]	 RdDin,																			//Agregar RdD a pipe
-				  input  logic [1:0]  MemRd, MemtoRegW, RegSrcD,											//Agregar MemRd, MemtoRegW, RegSrcD a pipe
-				  output logic [3:0]  WA3E, ALUControlEout,									//Agregar ALUControlE a pipe	
-				  output logic [4:0]  RdDout,													//Agregar RdD a pipe
+				  input  logic [3:0]	 WA3W, flagsEin,
+				  output logic [3:0]  WA3E, /*CondEPipeOutput,*/ flagsEout, ALUControlE,	
 				  output logic [31:0] RD1, RD2, Extended,
-				  output logic 		 CondEPipeOutput, ALUSrcEout, RegWriteDout, PlusOneout, MemSelEout);		//Agregar CondEPipeOutput, ALUSrcE, RegWriteD, PlusOne, MemSelE  a pipe
+				  output logic 		 ALUSrcE, MemToRegD, RegWriteD, PlusOne, BranchTakenE, PCSrcW);
 
 				  
 	//Instruction Mapping
 	
-	logic [1:0]  Op;
-	logic [5:0]  Funct;
-	logic [3:0]  Rd, CondE, decodeMux2x1ToRA1D_IN1, decodeMux2x1ToRA2D_IN1, decodeMux2x1ToRA2D_IN2;
+	logic        Op;
+	logic [3:0]  Funct;
+	logic [3:0]  /*CondE,*/ decodeMux2x1ToRA1D_IN1, decodeMux2x1ToRA2D_IN1, decodeMux2x1ToRA2D_IN2;
 	logic [23:0] ExtendedIN;
 	
-	assign Op 	 						= Instruction[27:26]; //Cambiar mapeo
-	assign Funct 						= Instruction[25:20]; //Cambiar mapeo
-	assign Rd    						= Instruction[15:12]; //Cambiar mapeo
-	assign CondE 						= Instruction[31:28];
+	//TODO ARREGLAR MAPEO
+	assign Op 	 						= Instruction[25];
+	assign Funct 						= Instruction[24:21];
+	//assign CondE 						= Instruction[31:28];
 	assign decodeMux2x1ToRA1D_IN1 = Instruction[19:16];
 	assign decodeMux2x1ToRA2D_IN1 = Instruction[3:0];
 	assign decodeMux2x1ToRA2D_IN2 = Instruction[15:12];
@@ -27,16 +24,15 @@ module decode(input  logic			 Clk, Rst, RegWriteW, ALUSrcEin, RegWriteDin, PlusO
 	
 	//Outputs
 	
-	logic [3:0]  decodeMux2x1ToRA1D_Output, decodeMux2x1ToRA2D_Output, decodeWA3E_Output, decodeALUControlE_Output;
+	logic [3:0]  decodeMux2x1ToRA1D_Output, decodeMux2x1ToRA2D_Output, decodeWA3E_Output, decodeALUControlE_Output, decodePipeALUControlE_Output, decodePipeFlagsE_Output;
 	logic [31:0] decodeExtended_Output, decodeRD1_Output, decodeRD2_Output, decodePipeRA_Output, decodePipeRB_Output, decodePipeExtended_Output;
-	logic [1:0]  decodeRegSrcD_Output, decodeMemRD_Output, decodeMemtoRegW_Output;
-	logic			 decodeALUSrcE_Output, decodeRegWriteD_Output, decodePlusOne_Output, decodeInmRegSel_Output, decodeMemSelE_Output;  //decodeInmRegSel_Output hay que cambiar el tamanyo y verificarlo No se si es de 1 bit o 2
-	logic [4:0]  decodeRdD_Output;
+	logic [1:0]  decodeRegSrcD_Output, decodeInmRegSel_Output;
+	logic			 decodeMemtoRegD_Output, decodeALUSrcE_Output, decodeRegWriteD_Output, decodePlusOne_Output, decodeBranchTake_Output, decodePCSrcW_Output, decodePipeRegWriteD_Output, decodePipePlusOne_Output, decodePipeBranchTake_Output, decodePipePCSrcW_Output, decodePipeALUSrcE_Output, decodePipeMemtoRegD_Output;
 	
 	//Components
 	
 	Control_unit
-	controlUnit(Clk, Funct, Op, Rd, decodeALUSrcE_Output, decodeALUControlE_Output, decodeMemRD_Output, decodeMemtoRegW_Output, decodeRegSrcD_Output, decodeRegWriteD_Output, decodeRdD_Output, decodePlusOne_Output, decodeInmRegSel_Output, decodeMemSelE_Output);
+	controlUnit(Funct, Op, decodeALUSrcE_Output, decodeALUControlE_Output, decodeMemtoRegD_Output, decodeRegSrcD_Output, decodeInmRegSel_Output, decodeRegWriteD_Output, decodePlusOne_Output, decodeBranchTake_Output, decodePCSrcW_Output);
 	
 	mux2x1
 	#(4)
@@ -53,20 +49,21 @@ module decode(input  logic			 Clk, Rst, RegWriteW, ALUSrcEin, RegWriteDin, PlusO
 	registerFileUnit(Clk, RegWriteW, decodeMux2x1ToRA1D_Output, decodeMux2x1ToRA2D_Output, WA3W, ResultW, PCPlus8D, decodeRD1_Output, decodeRD2_Output);
 
 	pipeDecoExe
-	pipeDE(Clk, Rst, decodeRD1_Output, decodePipeRA_Output, decodeRD2_Output, decodePipeRB_Output, decodeExtended_Output, decodePipeExtended_Output, decodeMux2x1ToRA2D_IN2, decodeWA3E_Output);
+	pipeDE(Clk, Rst, decodeRD1_Output, decodeRD2_Output, decodeExtended_Output, decodePipeRA_Output, decodePipeRB_Output, decodePipeExtended_Output, decodeMux2x1ToRA2D_IN2, decodeWA3E_Output, decodeALUControlE_Output, decodePipeALUControlE_Output, flagsEin, decodePipeFlagsE_Output, decodeRegWriteD_Output, decodePlusOne_Output, decodeBranchTake_Output, decodePCSrcW_Output, decodeALUSrcE_Output, decodeMemtoRegD_Output, decodePipeRegWriteD_Output, decodePipePlusOne_Output, decodePipeBranchTake_Output, decodePipePCSrcW_Output, decodePipeALUSrcE_Output, decodePipeMemtoRegD_Output);
 	
 	//Set output signals
 	
-	assign WA3E  	 = decodeWA3E_Output;
-	assign RD1  	 = decodePipeRA_Output;
-	assign RD2 		 = decodePipeRB_Output;
-	assign Extended = decodePipeExtended_Output;
-	//assign ALUControlEout = //;
-	//assign RdDout = //;
-	//assign CondEPipeOutput = //;
-	//assign ALUSrcEout = //;
-	//assign RegWriteDout = //;
-	//assign PlusOneout = //;
-	//assign MemSelEout = //;
+	assign WA3E  	 		  = decodeWA3E_Output;
+	assign RD1  	 		  = decodePipeRA_Output;
+	assign RD2 		 		  = decodePipeRB_Output;
+	assign Extended 		  = decodePipeExtended_Output;
+	assign flagsEout       = decodePipeFlagsE_Output;
+	assign ALUSrcE         = decodePipeALUSrcE_Output;
+	assign ALUControlE     = decodePipeALUControlE_Output;
+	assign MemToRegD       = decodePipeMemtoRegD_Output;
+	assign RegWriteD       = decodePipeRegWriteD_Output;
+	assign PlusOne         = decodePipePlusOne_Output;
+	assign BranchTakenE    = decodePipeBranchTake_Output;
+	assign PCSrcW          = decodePipePCSrcW_Output;
 
 endmodule 
