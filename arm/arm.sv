9b6f7c575efc0cce73f37fbc7ff6934af5a3dbb8
santigr17 <santigr17@gmail.com>
2018-09-30 22:35:29 -0600

    Comenzando con el procesador, uniendo partes

--- arm/arm.sv
+++ arm/arm.sv
@@ -1,11 +1,35 @@
 module arm(
-    input logic clk, reset,
-    output logic [31:0] PC,
-    input logic [31:0] Instr,
-    output logic MemWrite,
-    output logic [31:0] ALUResult, WriteData,
-    input logic [31:0] ReadData
-);
+   input logic clk,
+	 input logic [31:0] Instruction, ReadData,
+	 output logic [31:0] WriteData, AddressData, AddressInstru
 
+);
+  logic [31:0] ResultW;
+  logic [31:0] ALUOutM;
+  logic [31:0] ReadDataM;
+  logic MemToRegM, MemToRegW;
+  fetch stageFetch(
+        .clock(clk),
+        .mux1pin0(ResultW)
+  );
+  decode stageDeco(
+        .Clk(clk),
+  );
+  execute stageExe(
+        .Clk(clk),
+  );
+  memory stageMem(
+        .clock(clk),
+        .MemToRegOut(MemToRegW),
+        .ALUResultMOut(ALUOutM),
+        .ReadDataW(ReadDataM)
+  );
+// Write back stage
+  mux2x1 #(32) ResultWMux (
+        .a(ReadDataM),
+        .b(ALUOutM),
+        .ctrl(MemToRegW),
+        .y(ResultW)
+  );
 
 endmodule
--- arm/pipes/pipeMemWB.sv
+++ arm/pipes/pipeMemWB.sv
@@ -2,15 +2,17 @@ module pipeMemWB (input         clk,
 					  input  [31:0] RD,
 					  input  [31:0] ALUOutM,
 					  input  [3:0]  WA3M,
+						input  [3:0] MemToRegIn,
 					  output [31:0] ReadDataW,
 					  output [31:0] ALUOutW,
-					  output [3:0]  WA3W);
+					  output [3:0]  WA3W,
+						output [3:0] MemToRegOut);
 
 
 	logic [31:0] dataMemory;
 	logic [31:0] resultALU;
 	logic [3:0]  WAData;
-
+	logic [3:0]  MemToReg;
 
 	always_ff @(posedge clk)
 	begin
@@ -18,6 +20,7 @@ module pipeMemWB (input         clk,
 		dataMemory <= RD;
 		resultALU  <= ALUOutM;
 		WAData     <= WA3M;
+		MemToReg <= MemToRegIn;
 
 	end
 
@@ -27,6 +30,7 @@ module pipeMemWB (input         clk,
 		ReadDataW <= dataMemory;
 		ALUOutW   <= resultALU;
 		WA3W      <= WAData;
+		MemToRegOut <= MemToReg;
 
 	end
 
