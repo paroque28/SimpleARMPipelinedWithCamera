f4343a077117ea044cab4585533fb9320d34c037
santigr17 <santigr17@gmail.com>
2018-09-30 20:37:19 -0600

    memory stage ready

--- arm/pipes/pipeExeMem.sv
+++ arm/pipes/pipeExeMem.sv
@@ -1,17 +1,17 @@
-module pipeExeMem (input         clk, PCSrcMin, RegWriteMin, MemToRegMin,
+module pipeExeMem (input clk, PCSrcMin, RegWriteMin, MemToRegMin,
 					    input  [31:0] ALUResultE,
 					    input  [31:0] WriteDataE,
 					    input  [3:0]  WA3E,
 					    output [31:0] ADataMemory,
 					    output [31:0] WDDataMemory,
-					    output [3:0]  WA3M, CondEIn, CondEOut,
-						 output        PCSrcMout, RegWriteMout, MemToRegMout);
+					    output [3:0]  WA3M,
+						  output PCSrcMout, RegWriteMout, MemToRegMout);
 
 
 	logic [31:0] writeData;
 	logic [31:0] resultALU;
-	logic [3:0]  WAData, CondE;
-	
+	logic [3:0]  WAData;
+
 	logic        tmpPCSrcMinOut, tmpRegWriteMinOut, tmpMemToRegMinOut;
 
 
@@ -24,7 +24,7 @@ module pipeExeMem (input         clk, PCSrcMin, RegWriteMin, MemToRegMin,
 		tmpPCSrcMinOut    <= PCSrcMin;
 		tmpRegWriteMinOut <= RegWriteMin;
 		tmpMemToRegMinOut <= MemToRegMin;
-		CondE <= CondEIn;
+
 
 	end
 
@@ -37,7 +37,7 @@ module pipeExeMem (input         clk, PCSrcMin, RegWriteMin, MemToRegMin,
 		PCSrcMout    <= tmpPCSrcMinOut;
 		RegWriteMout <= tmpRegWriteMinOut;
 		MemToRegMout <= tmpMemToRegMinOut;
-		CondEOut <= CondE;
+
 
 	end
 
