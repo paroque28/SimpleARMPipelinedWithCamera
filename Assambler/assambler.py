431fc7940e8d7f36c0051f47fe5e62ae05da0757
DiSoJi <diegosolisj@gmail.com>
2018-10-11 23:31:16 -0600

    Implemented data instructions. add,mul,sub,lsl,lsr,thi,avg

--- Assambler/assambler.py
+++ Assambler/assambler.py
@@ -1,14 +1,15 @@
-op = {"nop":'{0:04b}'.format(0),
-    "mul":'{0:04b}'.format(1),
-    "sub":'{0:04b}'.format(2),
-    "ld":'{0:04b}'.format(3),
-    "add":'{0:012b}'.format(8),##4, remember to correct the rest 
+op = {"nop":'{0:04b}'.format(0b0000),#
+    "mul":'{0:04b}'.format(0b0001),#
+    "sub":'{0:04b}'.format(0b0010),#
+    "ld":'{0:04b}'.format(0b0011),
+    "add":'{0:04b}'.format(0b0100),#
+    ##"addi":'{0:012b}'.format(0b111000101001),
     "str":'{0:04b}'.format(5),
-    "lsl":'{0:04b}'.format(6),
-    "lsr":'{0:04b}'.format(7),
+    "lsl":'{0:04b}'.format(6),#
+    "lsr":'{0:04b}'.format(7),#
     "b":'{0:04b}'.format(8),
-    "pic":'{0:04b}'.format(9),
-    "avg":'{0:04b}'.format(10),
+    "pic":'{0:04b}'.format(9),#
+    "avg":'{0:04b}'.format(10),#
     "lda":'{0:04b}'.format(11),
     "orr":'{0:04b}'.format(12),
     "str_one":'{0:04b}'.format(13),
@@ -30,6 +31,14 @@ op = {"nop":'{0:04b}'.format(0),
     "r15":'{0:04b}'.format(15)}
 
 
+
+cond_do = '0b1110'
+cond_dont = '0b1111'
+add = ''
+add += cond_do + '00' + '1' + '0100' + '0'
+
+print(add)
+
 def branch_manager(token_list): ##In charge of managing the branch instructions
     code_line = op.get(token_list[0])
     adress = (token_list[1].replace("[","")).replace("]","")
@@ -41,27 +50,25 @@ def branch_manager(token_list): ##In charge of managing the branch instructions
         code_line += '{0:020b}'.format(0)
     return code_line
 
-
-def code_line_generator(token_list):
-    code_line = ""
+def code_line_manager_DATA(token_list):
+    code_line = ["1110", "00", "0", "", "1", " ", " ", " "]
     last_argument = token_list[-1]
-    if ("[" in last_argument) & ("#" in last_argument): ##Case where there is a [ and a # in the las token, must set flags according to
-        print("pop it") ##Meaningless print
-    elif ("[" in last_argument) & ("#" not in last_argument):##Case where there is a [ and not a # in the las token, must set flags according to
-        print("twist it") ##Meaningless print
-    elif ("[" not in last_argument) & ("#" in last_argument):##Case where there is a # and not a [ in the las token, must set flags according to
-        print("clap it") ##Meaningless print
-    else: 
-        for j in token_list:
-            ##print(op.get(j))
-            ##if '#' not in j: 
-            code_line += op.get(j)
-            ##else:
-            ##    token = j
-            ##    token = token.replace('#',"")
-    ##      ##        print(token)
-            ##    code_line += '{0:012b}'.format(int(token))
+    token_list[-1] = (last_argument.replace("#",""))
+    if token_list[0] == "avg":
+        code_line[3] = op.get(token_list[0])
+        code_line[5] = op.get(token_list[1])
+        code_line[6] = "0000"
+        code_line[7] = "000000000000"
         return code_line
+    elif "#" in last_argument:
+        code_line[2] = "1"
+        code_line[7] = str('{0:012b}'.format(int(token_list[-1])))
+    else:
+        code_line[7] = str('{0:08b}'.format(0)) + op.get(token_list[-1])
+    code_line[3] = op.get(token_list[0])
+    code_line[5] = op.get(token_list[1])
+    code_line[6] = op.get(token_list[2])
+    return code_line     
 
 def assambler(file):
     ##Reads lines from source file
@@ -86,91 +93,17 @@ def assambler(file):
             code_line += '{0:032b}'.format(0)
         elif first_argument == "pic":
             code_line += '{0:032b}'.format(9) ##Preguntar si esta lleva algún registro
-        elif first_argument == "thi":
-            code_line += '{0:032b}'.format(15)
+        ##elif first_argument == "thi":
+        ##    code_line += '{0:032b}'.format(15)
         elif (first_argument == "bne") | (first_argument == "beq") | (first_argument == "bgt"):
             code_line += branch_manager(tokens)
+        elif (first_argument == "ld") | (first_argument == "str") | (first_argument == "str_1") | (first_argument == "lda"):
+            print("mem_instruction")
         else:
-            code_line = code_line_generator(tokens)
-        code.append(code_line)
-    print(code)
+            ##code_line = code_line_manager(tokens)
+            print(code_line_manager_DATA(tokens))
 
-##def assambler(file):
-##    ##import re
-##
-##    ##listaOps = [*op]
-##    ##strOps = ""
-##    ##for i in listaOps:
-##    ##    strOps+=i
-##    ##    strOps+="|"
-####
-##    ##strOps = strOps[:-1]
-##    ##patron = re.compile('^('+strOps+') (r1[0-5]|r[0-9]|#[0-9]+)* (\[?r1[0-5]|r[0-9]|#[0-9]+\]?)?')
-##    ##patron2 = re.compile('^('+strOps+') (\[?r1[0-5]|r[0-9]|#[0-9]+\]?)')
-####Reads lines from source file
-##    with open(file) as fp:
-##        lines = fp.read().split("\n")
-##
-##    print("lines")
-##    print(lines)
-####Eliminates comas
-##    lines_n = []
-##    for i in lines:
-##        lines_n.append(i.replace(',',""))
-##    ##    print(i)
-##    ##print("lines_n")
-##    ##print(lines_n)
-##
-##    code = []
-##    for i in lines_n:
-##        tokens = i.split(" ")
-##        length = len(tokens)
-##        print("tokens")
-##        print(tokens)
-##        if length == 2:
-##            print(i)
-##            ##matcher = patron2.match(i)
-##            ##print("matcher.groups")
-##            ##print(matcher.groups())
-##            ##code_line = code_line_generator(matcher.groups())
-##            code_line = code_line_generator(tokens)
-##            code.append(code_line)
-##    ##        for j in matcher.groups():
-##    ##            ##print(op.get(j))
-##    ##        
-##    ##            if '#' not in j: 
-##    ##                code_line += op.get(j)
-##    ##            else:
-##    ##                token = j
-##    ##                token = token.replace('#',"")
-##    ####              print(token)
-##    ##                code_line += '{0:012b}'.format(int(token))
-##    ##        print(code_line)
-##    ##
-##        elif length == 1:
-##            code_line += '{0:032b}'.format(0)
-##        else:
-##            print(i)
-##            ##matcher = patron.match(i)
-##            ##print("matcher.groups")
-##            ##print(matcher.groups())
-##    ##      matcher.groups()
-##            code_line = ""
-##            ##code_line = code_line_generator(matcher.groups())
-##            code_line = code_line_generator(tokens)
-##            ##for j in matcher.groups():
-##            ##    ##print(op.get(j))
-##            ##
-##            ##    if '#' not in j: 
-##            ##        code_line += op.get(j)
-##            ##    else:
-##            ##        token = j
-##            ##        token = token.replace('#',"")
-##    ##      ##        print(token)
-##            ##        code_line += '{0:012b}'.format(int(token))
-##            ##        
-##            code.append(code_line)
-##            print(code_line)
-##    print(code)
+        ##code.append(code_line)
+    print(code)
 
 assambler('instructions.txt')
